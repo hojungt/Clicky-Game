47983e8b69c3a83ab387ec7b88ec561bf78081e7
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-06-02 22:34:44 -0400

    edit card src and onClick, edit game.js methods, retrieve serviceWorker.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,19 @@ import React from "react";
 function Card(props) {
     return (
         <img 
-            className="card-img"
+            className="img-fluid img-thumbnail"
             alt={props.name}
-            src={props.image}
+            // to find images locally, must write path and `require` in `{}`
+            src={require("../images/" + props.image)}
+            style={style}
+            onClick={() => props.checkGuess(props.id)}
         />
     );
-}
+};
+
+const style = {
+    maxHeight: "200px",
+    maxWidth: "200px"
+};
 
 export default Card;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -9,14 +9,49 @@ import Row from "../components/Row";
 import images from "../images/images.json"
 
 export class Game extends Component {
+    // define state
     state = {
+        images: images,
+        imagesUnguessed: images,
         winScore: 0,
         topScore: 0,
         message: "Click an image to begin",
+    };  
+
+    // shuffle by array.sort
+    shuffleArray = (array) => {
+        array.sort(() => Math.random - 0.5);
+    };
+    // `sort()` sorts the elements of an array in place and returns the sorted array
+    // The default sort order is built upon converting the elements into strings, 
+    // then comparing their sequences of UTF-16 code units values.
+
+
+    // change to this state if the image hasn't been guessed
+    setStateGuessRight = (imagesNew) => {
+        this.setState({
+            images: images,
+            imagesUnguessed: imagesNew,    
+            winScore: this.state.score +1,
+            message: "Your guess is right!"
+        })
     };
 
+    // change to this state if the image has already been guessed
+    setStateGuessWrong = () => {
+        this.setState({
+            images: images,
+            imagesUnguessed: images,    
+            topScore: this.state.winScore > this.state.topScore ? this.state.winScore : this.state.topScore,
+            winScore: 0,
+            message: "Game over! Your guess is wrong."
+        })
+    };
 
-
+    // check if the image has been guessed, compared to original images.json
+    checkGuess = (id) => {
+        
+    };
 
     render() {
         return (
@@ -30,8 +65,13 @@ export class Game extends Component {
 
                 <Container>
                     <Row>
-                        <Card 
-                        />
+                        {this.state.images.map(image => 
+                            (<Card 
+                                id={image.id}
+                                name={image.name}
+                                image={image.image}
+                            />
+                        ))}
                     </Row>
                 </Container>
 
